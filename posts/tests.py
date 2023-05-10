1e16d4e0b247405441ed2e92f8452308bb088e38
Anna Gabain <anna.gabain@outlook.com>
2023-05-10 18:44:22 +0000

    Add Post tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+# from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='anna', password='pass')
+
+    def test_can_list_posts(self):
+        anna = User.objects.get(username='anna')
+        Post.objects.create(owner=anna, title='some title')
+        response = self.client.get('/posts/')
+        # deliberately failed
+        # self.assertEqual(response.status_code, status.HTTP_200_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('=============================')
+        print('response data: ', response.data)
+        print('response data length (is a single post):', len(response.data))
+    
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='anna', password='pass')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        # deliberately failed
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_cant_create_post(self):
+        response = self.client.post('/posts/')
+        # deliberately failed
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
