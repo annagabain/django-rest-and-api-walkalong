64bdf4d07513aae9f81014184678c683926b7e8e
Anna Gabain <anna.gabain@outlook.com>
2023-05-19 15:09:56 +0000

    Preparing the API for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,8 +30,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -51,7 +59,7 @@ SECRET_KEY = 'django-insecure-)d5s^+k8p*s5mzectti3f#=eg^c4a3%b*oy_v46*c)ed0)etb7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-annagabain-djangorestan-gfqcw8zlr07.ws-eu97.gitpod.io',]
 
 
 # Application definition
